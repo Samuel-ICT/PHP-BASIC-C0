40c6490e616c9f6bc8480f8132681430d9c49caa
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-25 14:40:50 +0100

    5 datatypes

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,15 @@
+<?php
+$voornaam = "Tom";
+echo gettype($voornaam);
+echo "<br>";
+$melkprijs = 0.98;
+echo gettype($melkprijs);
+echo "<br>";
+$leeftijd = 19;
+echo gettype($leeftijd);
+echo "<br>";
+$true = true;
+echo gettype($true);
+echo "<br>";
+$positie = null;
+echo gettype($positie);
