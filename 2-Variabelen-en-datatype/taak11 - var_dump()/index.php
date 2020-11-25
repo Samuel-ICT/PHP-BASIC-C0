3a65443583922a49032e4b6100f93ffa1df1f97f
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-25 15:46:23 +0100

    test var_dump

---
+++ 2-Variabelen-en-datatype/taak11 - var_dump()/index.php
@@ -0,0 +1,12 @@
+<?php
+$voornaam="Samuel";
+var_dump($voornaam); 
+echo "<br>";
+$leeftijd = 19;
+var_dump($leeftijd);
+echo "<br>";
+$bolean = true;
+var_dump($bolean);
+echo "<br>";
+$melkprijs = 0.98;
+var_dump($melkprijs);
