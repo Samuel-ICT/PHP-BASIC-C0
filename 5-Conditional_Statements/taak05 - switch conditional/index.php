e576dbd9ad7be88e0203adb772274f0ea94447c0
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-30 17:20:19 +0100

    conditional statement switch

---
+++ 5-Conditional_Statements/taak05 - switch conditional/index.php
@@ -0,0 +1,28 @@
+<?php
+$score = 100;
+
+switch($score){
+    case 100:
+    echo "Wow wat een fantastische score!";
+    echo "<br>";
+    case 90:
+    echo "Wow bijna alle punten nog 10 te gaan!";
+    echo "<br>";
+
+    case 80:
+
+    echo "Goed bezig, bijna alle punten!";
+
+    echo "<br>";
+
+    case 70:
+
+    echo "Ga zo door!";
+
+    echo "<br>";
+
+    default:
+
+    echo "Nog even oefenen dan haal je vast alle punten!";
+    }
+?>
