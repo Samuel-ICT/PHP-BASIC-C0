54cfbfe655b49b9ae3216a6239c50133f933b245
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-30 12:08:21 +0100

    arithmitic operators

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,11 @@
+<?php
+$getal1 = 100;
+$getal2 = 50;
+echo $getal1 + $getal2 ;
+echo "<br>";
+echo $getal1 - $getal2;
+echo "<br>";
+echo $getal1 * $getal2; 
+echo "<br>";
+echo $getal1 / $getal2;
+?>
