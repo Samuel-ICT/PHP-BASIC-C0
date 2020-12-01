2891ccb53dea6414a62bfe83632448bdf10bb7dd
Samuel Adu <samuel.adu@student.rocva.nl>
2020-12-01 18:30:31 +0100

    require

---
+++ 7-Working with files/taak02 - require/includes/menu.php
@@ -0,0 +1,5 @@
+<?php
+echo "<li>menu-item 1</li>";
+echo "<li>menu-item 2</li>";
+echo "<li>menu-item 3</li>";
+?>
---
+++ 7-Working with files/taak02 - require/index.php
@@ -0,0 +1,4 @@
+<?php
+require "includes/database.php";
+include "includes/menu.php";
+?>
