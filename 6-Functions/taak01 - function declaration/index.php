2fa845e2c516c5b17f08697d64099229be38cc22
Samuel Adu <samuel.adu@student.rocva.nl>
2020-12-01 11:32:22 +0100

    functions

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,8 @@
+<?php
+function zetTweeWoordenAanElkaar(){ 
+    $woordje = "Hallo";
+    $zinnetje = " mooie blauwe planeet!";
+    echo $helezin = $woordje . $zinnetje;
+}
+zetTweeWoordenAanElkaar();
+?>
