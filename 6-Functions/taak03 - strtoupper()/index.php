7b87f752ec888cad831b735ae3b71fcb043e91f0
Samuel Adu <samuel.adu@student.rocva.nl>
2020-12-01 11:47:40 +0100

    multiple function

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,8 @@
+<?php
+function telAlleWoorden(){
+    $zinnetje = "Mijn blauwe auto is een snelle mooie wagen";
+     $lengteZin = strlen($zinnetje); 
+     echo $lengteZin;
+}
+telAlleWoorden(); 
+?>
