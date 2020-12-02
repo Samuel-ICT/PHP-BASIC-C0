bedc7ad62481df36ff39884ea88c8694b104abe1
Samuel Adu <samuel.adu@student.rocva.nl>
2020-12-02 16:47:17 +0100

    html & php

---
+++ 7-Working with files/taak03 - html & php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<div id="isMeerderJarig">
+        <?php
+        $leeftijd = 19;
+            if($leeftijd < 19 ){
+                echo "<h6>Je bent te jong om deze pagina te bekijken</h6>";
+            } else{
+                echo "<h3>Je bent oud genoeg om deze pagina te bekijken</h3>";
+            }
+        ?>
+</div>
+</body>
+</html>
